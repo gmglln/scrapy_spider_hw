e5d8a3ab323ed4132479e54b4ed13721d5e95614
Memo Mogollan <gmogollan@gmail.com>
2022-06-13 13:38:53 -0500

    Update en el script principal

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,9 +1,12 @@
-import re
+from email.quoprimime import quote
+from gc import callbacks
+import json
 import scrapy
 
 # Titulo = //h1/a/text()
 # Citas = //span[@class="text" and @itemprop="text"]/text()
 # Top ten tags = //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
+# Next page btn = //ul[@class="pager"]//li[@class="next"]/a/@href
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
@@ -11,16 +14,34 @@ class QuotesSpider(scrapy.Spider):
         'http://quotes.toscrape.com/page/1/'
     ]
 
+    custom_settings = {
+      'FEED_URI': 'quoes.json',
+      'FEED_FORMAT': 'json'
+    }
+
+    def parse_only_quotes(self, response, **kwargs):
+      if kwargs:
+        quotes = kwargs['quotes']
+      quotes.extend(response.xpath('//span[@class="text" and @itemprop="text"]/text()')).getall()
+
+      next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+      if next_page_button_link:
+        yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
+      else:
+        yield {
+          'quotes': quotes
+        }
+
     def parse(self, response):
-        
-        # definimos nuestros xpaths
         title = response.xpath('//h1/a/text()').get()
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
         top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
         
-        # Create json
         yield {
           'title': title,
-          'quotes': quotes,
           'top_ten_tags': top_ten_tags
         }
+
+        next_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        if next_page_button_link:
+          yield response.follow(next_page_button_link, callback=self.parse_only_quotes, cb_kwargs={'quotes': quotes})
